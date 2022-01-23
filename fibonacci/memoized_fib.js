137b8cbffcc3cdc5c526162dcfa195d8e98f9956
Brandon Ellis <brandonellis.email@gmail.com>
2022-01-23 16:54:23 -0700

    init

---
+++ fibonacci/memoized_fib.js
@@ -0,0 +1,14 @@
+
+
+const fib = (num) => {
+    // base case: if the number is 1 or 2, return 1 since the first two nums are 1 
+    if (num <= 2) return 1;
+    // recurse with the num below and 2 nums below
+    return fib(num - 1) + fib(num - 2);
+};
+
+let test1 = fib(7);
+let test2 = fib(10);
+
+console.log(test1);
+console.log(test2);
