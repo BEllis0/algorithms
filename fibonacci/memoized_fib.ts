6bf3f9946a39d1a2e6a4506237a781b56e620353
Brandon Ellis <brandonellis.email@gmail.com>
2022-02-12 15:06:22 -0700

    converted to ts

--- fibonacci/memoized_fib.ts
+++ fibonacci/memoized_fib.ts
@@ -1,14 +1,10 @@
 
-// fibonacci function takes a number (place in the fib sequence)
-const fib = (num) => {
+
+const fibMemo = (num: number):number => {
     // base case: if the number is 1 or 2, return 1 since the first two nums are 1 
     if (num <= 2) return 1;
     // recurse with the num below and 2 nums below
     return fib(num - 1) + fib(num - 2);
 };
 
-let test1 = fib(7);
-let test2 = fib(10);
-
-console.log(test1);
-console.log(test2);
+module.exports.fibMemo = fibMemo;
